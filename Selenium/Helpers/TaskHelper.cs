aaf6c138445282c7f8f6cfd77d797b7d95f8e301
Focus <maniaclord@bk.ru>
2022-10-27 16:05:27 +0300

    AddTaskTest Fixed

--- Selenium/Helpers/TaskHelper.cs
+++ Selenium/Helpers/TaskHelper.cs
@@ -1,5 +1,4 @@
-﻿using OpenQA.Selenium;
-using Selenium.Entities;
+﻿using Selenium.Entities;
 
 namespace Selenium.Helpers;
 
@@ -13,13 +12,9 @@ public class TaskHelper : HelperBase
     {
         FindElement(FindBy.CssSelector, ".plus_add_button").Click();
 
-        FindElement(FindBy.CssSelector, ".public-DraftStyleDefault-block").Click();
-        {
-            var element = FindElement(FindBy.CssSelector, ".notranslate");
-            _app.JavaScriptExecutor.ExecuteScript(
-                $"if(arguments[0].contentEditable === 'true') {{arguments[0].innerText = '<div data-contents=\"true\"><div class=\"\" data-block=\"true\" data-editor=\"a5f1g\" data-offset-key=\"adcnk-0-0\"><div data-offset-key=\"adcnk-0-0\" class=\"public-DraftStyleDefault-block public-DraftStyleDefault-ltr\"><span data-offset-key=\"adcnk-0-0\"><span data-text=\"true\">TEXTEXTEXTEXT</span></span></div></div></div>'}}",
-                element);
-        }
+        FindElement(FindBy.CssSelector, ".notranslate.public-DraftEditor-content").Click();
+        FindElement(FindBy.CssSelector, ".notranslate.public-DraftEditor-content").Fill(task.Title);
+
         FindElement(FindBy.CssSelector, ".task_editor__description_field").Click();
         FindElement(FindBy.CssSelector, ".task_editor__description_field").Fill(task.Text);
         FindElement(FindBy.CssSelector, ".\\_3d1243b2 > .bbdb467b").Click();
